b52f8a00f40e91790613526acfa7eb78e9b525f3
kpcruz <kelvinmembership@gmail.com>
2018-11-12 14:30:21 -0800

    inital framework added

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) values ('Cheeseburger', 0);
+INSERT INTO burgers (burger_name, devoured) values ('Double Quarter Pounder', 0);
+INSERT INTO burgers (burger_name, devoured) values ('Big Mac', 0);
---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
